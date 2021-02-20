6de1902d3fb08dbb713123b4296e6cb338b1bbdb
Z3narchy <seb1218@hotmail.fr>
2021-02-20 07:26:37 -0500

    ajout version seb

---
+++ StationMeteoSeb/src/CredentialsCourtierDeMessages.h
@@ -0,0 +1,4 @@
+const char *mqttServer = "192.168.43.198";
+const int mqttPort = 1883;
+const char *mqttUser = "homeassistant";
+const char *mqttPassword = "ohZ0Ualoo3bae7thie4Nookase2aghaeph5ap4yei2ADae6NeewohthohFee1ohN";
